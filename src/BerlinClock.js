818ca75ed282a316830726404d2ba0e544dcd60c
diana.potupis <diana.potupis@student.vinci.be>
2024-11-08 09:56:52 +0100

    refactor: created the loop

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -1,16 +1,12 @@
 export class Main {
-    simpleMinutesLine(minutes){
-        minutes = minutes % 5;
-        const patterns = ["O O O O", "Y O O O", "Y Y O O", "Y Y Y O", "Y Y Y Y"];
-        if(minutes == 1){
-            return patterns[1];
-        } else if(minutes == 2){
-            return patterns[2];
-        } else if(minutes == 3){
-            return patterns[3];
-        } else if(minutes == 4){
-            return  patterns[4];
-        }
-        return patterns[0];
+  simpleMinutesLine(minutes) {
+    minutes = minutes % 5;
+    const patterns = ["O O O O", "Y O O O", "Y Y O O", "Y Y Y O", "Y Y Y Y"];
+
+    for (let i = 0; i < patterns.length; i++) {
+      if (i == minutes) {
+        return patterns[i];
+      }
     }
+  }
 }
