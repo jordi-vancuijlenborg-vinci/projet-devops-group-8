6053e69898818369bf5d9378d042422a1bec5b4d
diana.potupis <diana.potupis@student.vinci.be>
2024-11-08 10:00:06 +0100

    refactor: simplified the function

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -1,12 +1,9 @@
 export class Main {
   simpleMinutesLine(minutes) {
     minutes = minutes % 5;
+
     const patterns = ["O O O O", "Y O O O", "Y Y O O", "Y Y Y O", "Y Y Y Y"];
 
-    for (let i = 0; i < patterns.length; i++) {
-      if (i == minutes) {
-        return patterns[i];
-      }
-    }
+    return patterns[minutes];
   }
 }
