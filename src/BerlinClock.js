f608d9e4ed67c5fc99786916cdda5e90d8cfe693
Ranya <ranya.houmaid@student.vinci.be>
2024-11-09 13:37:39 +0100

    refactor: simplified the functions

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -12,40 +12,7 @@ export class Main {
 
     const patterns = ["O O O O O O O O O O O", "Y O O O O O O O O O O", "Y Y O O O O O O O O O", "Y Y Y O O O O O O O O", "Y Y Y Y O O O O O O O", "Y Y Y Y Y O O O O O O","Y Y Y Y Y Y O O O O O","Y Y Y Y Y Y Y O O O O","Y Y Y Y Y Y Y Y O O O","Y Y Y Y Y Y Y Y Y O O","Y Y Y Y Y Y Y Y Y Y O","Y Y Y Y Y Y Y Y Y Y Y"];
 
-    if (minutes===1){
-      return patterns[1];
-    }
     
-    if (minutes===2){
-      return patterns[2];
-    }
-    if (minutes===3){
-      return patterns[3];
-    }
-    if (minutes===4){
-      return patterns[4];
-    }
-    if (minutes===5){
-      return patterns[5];
-    }
-    if (minutes===6){
-      return patterns[6];
-    }
-    if (minutes===7){
-      return patterns[7];
-    }
-    if (minutes===8){
-      return patterns[8];
-    }
-    if (minutes===9){
-      return patterns[9];
-    }
-    if (minutes===10){
-      return patterns[10];
-    }
-    if (minutes===11){
-      return patterns[11];
-    }
-    return patterns[0];
+    return patterns[minutes];
   }
 }
