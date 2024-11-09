2e1b0c6a8f2fd15e8bc1065fdb3682addd9b8429
Ranya <ranya.houmaid@student.vinci.be>
2024-11-09 13:35:30 +0100

    refactor: create table 5MinutesLines

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -8,39 +8,44 @@ export class Main {
   }
 
   blocDe5Minutes(minutes) {
+    minutes = minutes % 12;
+
+    const patterns = ["O O O O O O O O O O O", "Y O O O O O O O O O O", "Y Y O O O O O O O O O", "Y Y Y O O O O O O O O", "Y Y Y Y O O O O O O O", "Y Y Y Y Y O O O O O O","Y Y Y Y Y Y O O O O O","Y Y Y Y Y Y Y O O O O","Y Y Y Y Y Y Y Y O O O","Y Y Y Y Y Y Y Y Y O O","Y Y Y Y Y Y Y Y Y Y O","Y Y Y Y Y Y Y Y Y Y Y"];
+
     if (minutes===1){
-      return "Y O O O O O O O O O O"
+      return patterns[1];
     }
+    
     if (minutes===2){
-      return "Y Y O O O O O O O O O"
+      return patterns[2];
     }
     if (minutes===3){
-      return "Y Y Y O O O O O O O O"
+      return patterns[3];
     }
     if (minutes===4){
-      return "Y Y Y Y O O O O O O O"
+      return patterns[4];
     }
     if (minutes===5){
-      return "Y Y Y Y Y O O O O O O"
+      return patterns[5];
     }
     if (minutes===6){
-      return "Y Y Y Y Y Y O O O O O"
+      return patterns[6];
     }
     if (minutes===7){
-      return "Y Y Y Y Y Y Y O O O O"
+      return patterns[7];
     }
     if (minutes===8){
-      return "Y Y Y Y Y Y Y Y O O O"
+      return patterns[8];
     }
     if (minutes===9){
-      return "Y Y Y Y Y Y Y Y Y O O"
+      return patterns[9];
     }
     if (minutes===10){
-      return "Y Y Y Y Y Y Y Y Y Y O"
+      return patterns[10];
     }
     if (minutes===11){
-      return "Y Y Y Y Y Y Y Y Y Y Y"
+      return patterns[11];
     }
-    return "O O O O O O O O O O O";
+    return patterns[0];
   }
 }
